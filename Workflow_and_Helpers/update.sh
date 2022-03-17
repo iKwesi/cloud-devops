09454cd9c98a099592153aeda2c9f65127f45699
iKwesi <rbblankson34@gmail.com>
2022-03-18 00:28:55 +0700

    removed files

--- Workflow_and_Helpers/update.sh
+++
@@ -1,7 +0,0 @@
-# the dollar signs are variables thus allows you to pass the stack name, filepath and parameters to commandline 
-# use this script to save time writing all the commands
-aws cloudformation update-stack \
---stack-name $1 \
---template-body file://$2 \
---parameters file://$3 \
---region=us-west-2
